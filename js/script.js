b3af3bbdc88f5e3acc2aa14d851c69f1f397a184
Andika Ega Hutama <andikaega720@gmail.com>
2025-12-18 23:41:58 +0700

    change image to webp and cleaning code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-// const promptUser = prompt("Please input your name", "");
-
-// document.getElementById("name").innerHTML = promptUser;
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const prompter = prompt("Input your name:", "");
-
-//   if (prompter == null || prompter == "") {
-//     return prompter;
-//   }
-//   document.getElementById("name").innerHTML = prompter;
-//   document.getElementsByClassName("headerText")[0].style.display = "inline";
-// });
-
 function fillName() {
   const prompter = prompt(
     "Welcome to Taka Company, Please Input your name: ",
